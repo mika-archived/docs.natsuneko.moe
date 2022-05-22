eba6271456918ca75e12f77142f4b3e36e313b83
Natsune Mochizuki <me@natsuneko.cat>
2022-05-22 16:25:48 +0900

    fix: fix middleware

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -2,17 +2,25 @@ import { NextRequest, NextResponse } from "next/server";
 
 const PUBLIC_FILE = /\.(.*)$/;
 
+const LOCALE_MAP = {
+  "ja-jp": "ja-JP",
+  "en-us": "en-US",
+};
+
 const middleware: (request: NextRequest) => NextResponse | undefined = (
   request: NextRequest
 ) => {
   const shouldHandleLocale =
-    !PUBLIC_FILE.test(request.nextUrl.pathname) &&
-    !request.nextUrl.pathname.includes("/api/") &&
-    request.nextUrl.locale === "default";
+    (!PUBLIC_FILE.test(request.nextUrl.pathname) &&
+      !request.nextUrl.pathname.includes("/api/") &&
+      request.nextUrl.locale === "default") ||
+    !!LOCALE_MAP[request.nextUrl.locale];
+
+  const locale = LOCALE_MAP[request.nextUrl.locale.toLowerCase()] ?? "en-US";
 
   return shouldHandleLocale
     ? NextResponse.redirect(
-        new URL(`/ja-JP/${request.nextUrl.pathname}`, request.url)
+        new URL(`/${locale}/${request.nextUrl.pathname}`, request.url)
       )
     : NextResponse.next();
 };
